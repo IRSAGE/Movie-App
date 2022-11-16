a7e071147f2f0976ac4391f4c09b5d0f796af59a
IRSAGE <sibo.egide98@gmail.com>
2022-11-16 19:28:14 +0200

    Configuring Sass

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/"><h1>Movie App</h1></NuxtLink>
+      </nav>
+    </header>
+    <div><slot /></div>
+  </div>
+</template>
+
+<style lang="sass" scoped></style>
