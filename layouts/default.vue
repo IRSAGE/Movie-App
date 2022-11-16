e2042256c32a8c914ed04d603fae163215348def
IRSAGE <sibo.egide98@gmail.com>
2022-11-16 21:45:41 +0200

    Designing the header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>
-    <header>
+  <div class="app">
+    <header class="header">
       <nav>
-        <NuxtLink to="/"><h1>Movie App</h1></NuxtLink>
+        <NuxtLink class="link" to="/">Movie App</NuxtLink>
       </nav>
     </header>
     <div><slot /></div>
   </div>
 </template>
 
-<style lang="sass" scoped></style>
+<style lang="scss" scoped>
+.header {
+  background-color: #013d5b;
+  display: flex;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 64px;
+  width: 100%;
+  z-index: 10;
+  transition: top 0.2s linear;
+  nav {
+    margin: 10px;
+    align-items: center;
+  }
+  .link {
+    text-decoration: none;
+    color: white;
+    font-weight: bold;
+    font-size: 25px;
+  }
+}
+</style>
