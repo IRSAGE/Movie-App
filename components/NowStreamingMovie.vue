7513e6aff980219bcd7a36d46e6e1f6b29d0eea5
IRSAGE <sibo.egide98@gmail.com>
2022-11-21 10:37:53 +0200

    Refactoring the code base for reusabilty

---
+++ components/NowStreamingMovie.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="movie-grid" class="movies-grid">
+    <div class="movie" v-for="(movie, index) in movies" :key="index">
+      <MovieComp :movie="movie" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NowStreamingMovie",
+  props: ["movies"],
+};
+</script>
+
+<style lang="scss">
+.movies-grid {
+  display: grid;
+  column-gap: 40px;
+  row-gap: 64px;
+  margin: 25px;
+  grid-template-columns: 1fr;
+  @media (min-width: 500px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (min-width: 750px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (min-width: 1100px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+  .movie {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    background-color: #013d5b;
+    border-radius: 15px;
+    height: 500px;
+  }
+}
+</style>
